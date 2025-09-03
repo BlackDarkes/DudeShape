b32120df206ae45ae2c888aa718d285f9bdb0121
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-09-04 01:08:59 +0500

    Сделана корзина пользователя

--- api/src/modules/cart/cart.controller.ts
+++ api/src/modules/cart/cart.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CartService } from './cart.service';
 
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
+
+  @Get(":id")
+  async showUserCart(@Param("id") id: string) {
+    return this.cartService.showUserCart(id);
+  }
+
+  @Post()
+  async create(@Body("userId") userId: string, @Body("furnitureId") furnitureId: string, @Body("quantity") quantity: number) {
+    return this.cartService.create(userId, furnitureId, quantity);
+  }
+
+  @Patch(":id")
+  async update(@Param("id") id: string, @Body("quantity") quantity: number) {
+    return this.cartService.updateCart(id, quantity);
+  }
+
+  @Delete(":id")
+  async delete(@Param("id") id: string) {
+    return this.cartService.deleteCart(id);
+  }
 }
--- api/src/modules/cart/cart.service.ts
+++ api/src/modules/cart/cart.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
+import { CartEntity } from './entities/cart.entity';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(CartEntity)
+    private readonly cartRepository: Repository<CartEntity>,
+  ) {}
+
+  async showUserCart(id: string) {
+    const userCart = this.cartRepository.find({ where: { userId: id } });
+    return userCart;
+  }
+
+  async create(userId: string, furnitureId: string, quantity: number) {
+    await this.cartRepository.save({
+      userId,
+      furnitureId,
+      quantity
+    });
+
+    return {
+      message: "Товар добавлен в корзину!",
+    }
+  }
+
+  async updateCart(id: string, quantity: number) {
+    await this.cartRepository.update({ id }, { quantity });
+    return {
+      message: "Количество изменилось!"
+    }
+  }
+
+  async deleteCart(id: string) {
+    await this.cartRepository.delete(id);
+    return {
+      message: "Товар удален из корзины!"
+    }
+  }
+}
