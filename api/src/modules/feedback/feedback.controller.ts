724f8add738b69939d1ba8df4beee09529d6d39c
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-09-04 00:19:14 +0500

    Сделано feedback

--- api/src/modules/feedback/feedback.controller.ts
+++ api/src/modules/feedback/feedback.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 
 @Controller('feedback')
 export class FeedbackController {
   constructor(private readonly feedbackService: FeedbackService) {}
+
+  @Get(":id")
+  async getById(@Param("id") id: string) {
+    return this.feedbackService.getUserFeedback(id);
+  }
+
+  @Post()
+  async getAllFeedbacks() {
+    return this.feedbackService.getAllFeedbackUsers();
+  }
 }
--- api/src/modules/feedback/feedback.service.ts
+++ api/src/modules/feedback/feedback.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../user/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { FeedbackEntity } from './entities/feedback.entity';
+import { useContainer } from 'class-validator';
 
 @Injectable()
-export class FeedbackService {}
+export class FeedbackService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FeedbackEntity)
+    private readonly feedbackRepository: Repository<FeedbackEntity>,
+  ) {}
+
+  async getUserFeedback(userId: string) {
+    const userFeedbacks = this.userRepository
+      .createQueryBuilder("users")
+      .select(["users.id as id", "users.name as name", "feedbacks.message as message", "feedbacks.stars as stars"])
+      .innerJoin("users.feedbacks", "feedbacks")
+      .where("users.id = :id", { id: userId })
+      .getRawOne()
+
+    return userFeedbacks;
+  };
+
+  async getAllFeedbackUsers() {
+    const usersFeedback = this.userRepository
+      .createQueryBuilder("users")
+      .select(["users.id as id", "users.name as name", "feedbacks.message as message", "feedbacks.stars as stars"])
+      .innerJoin("users.feedbacks", "feedbacks")
+      .getRawMany()
+
+    return usersFeedback
+  }
+} 
