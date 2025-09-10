1fa2ca454a3f480bd8a6db41f310427783b4effe
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2025-09-11 00:25:20 +0500

    Сделана прокрутка у компаний

--- frontend/src/widgets/companies/ui/Companies/Companies.tsx
+++ frontend/src/widgets/companies/ui/Companies/Companies.tsx
@@ -1,14 +1,48 @@
+"use client";
+
+import { useRef, useEffect } from 'react';
 import { Container } from "@/shared/ui/Container/Container";
 import { CompaniesList } from "../CompaniesList/CompaniesList";
+import styles from './Companies.module.scss';
 
 export const Companies = () => {
-  return (
-    <section>
-      <Container>
-        <h1>Trusted by 20,000+ companies</h1>
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+  const scrollSpeed = isMobile ? 0.6 : 1;
 
-        <CompaniesList />
+  useEffect(() => {
+    const scroll = scrollRef.current;
+
+    if (!scroll) return;
+
+    let scrollLeft = 0;
+    let animateFrameId: number;
+
+    const animate = () => {
+      scrollLeft += scrollSpeed;
+      if (scrollLeft >= scroll.scrollWidth / 2) {
+        scrollLeft = 0;
+      };
+      scroll.scrollLeft = scrollLeft;
+      animateFrameId = requestAnimationFrame(animate);
+    };
+
+    animateFrameId = requestAnimationFrame(animate);
+    return () => cancelAnimationFrame(animateFrameId);
+  }, [scrollSpeed])
+
+  return (
+    <section className={styles.companies}>
+      <Container className={styles.companiesContainer}>
+        <h1 className={styles.companiesTitle}>Trusted by 20,000+ companies</h1>
+        <div
+          className={styles.companiesList}
+          ref={scrollRef}
+        >
+          <CompaniesList />
+          <CompaniesList />
+        </div>
       </Container>
     </section>
   );
-}
+};
--- frontend/src/widgets/companies/ui/CompaniesList/CompaniesList.tsx
+++ frontend/src/widgets/companies/ui/CompaniesList/CompaniesList.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { companiesImages } from "../../constants/companiesImages";
+import styles from './CompaniesList.module.scss'
 
 export const CompaniesList = () => {
   return (
-    <ul>
+    <ul className={styles.list}>
       {companiesImages.map((image) => (
-        <li key={image.id}>
+        <li key={image.id} className={styles.listItem}>
           <Image src={image.src} alt="компания" width={image.width} height={image.height} />
         </li>
       ))}
